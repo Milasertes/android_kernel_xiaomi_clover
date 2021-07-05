1c1e719d3ff341f15725d791eb41bbe955a8880b
Milasertes <67229603+Milasertes@users.noreply.github.com>
2021-07-05 08:20:38 +0000

    Update sdm660.sh

--- sdm660.sh
+++ sdm660.sh
@@ -1,17 +1,17 @@
 starttime=`date +'%Y-%m-%d %H:%M:%S'`
 
 git submodule update --init --recursive
-export PATH=/home/lu/clang/bin:${PATH}
+export PATH=/home/milasertes/clang/bin:${PATH}
 make O=out ARCH=arm64 clover_defconfig -j$(nproc --all) LLVM=1
-make LLVM=1 -j$(nproc --all) O=out \ARCH=arm64 \CC=/home/lu/clang/bin/clang \
-	CROSS_COMPILE=/home/lu/clang/bin/aarch64-linux-gnu- \
-	CROSS_COMPILE_ARM32=/home/lu/clang/bin/arm-linux-gnueabi- \
-	CROSS_COMPILE_COMPAT=/home/lu/clang/bin/arm-linux-gnueabi- \
-	OBJCOPY=/home/lu/clang/bin/llvm-objcopy \
-	OBJDUMP=/home/lu/clang/bin/llvm-objdump \
-	STRIP=/home/lu/clang/bin/llvm-strip \
-	NM=/home/lu/clang/bin/llvm-nm \
-	AR=/home/lu/clang/bin/llvm-ar 2>&1 | tee kernel.log\
+make LLVM=1 -j$(nproc --all) O=out \ARCH=arm64 \CC=/home/milasertes/clang/bin/clang \
+	CROSS_COMPILE=/home/milasertes/clang/bin/aarch64-linux-gnu- \
+	CROSS_COMPILE_ARM32=/home/milasertes/clang/bin/arm-linux-gnueabi- \
+	CROSS_COMPILE_COMPAT=/home/milasertes/clang/bin/arm-linux-gnueabi- \
+	OBJCOPY=/home/milasertes/clang/bin/llvm-objcopy \
+	OBJDUMP=/home/milasertes/clang/bin/llvm-objdump \
+	STRIP=/home/milasertes/clang/bin/llvm-strip \
+	NM=/home/milasertes/clang/bin/llvm-nm \
+	AR=/home/milasertes/clang/bin/llvm-ar 2>&1 | tee kernel.log\
 
 endtime=`date +'%Y-%m-%d %H:%M:%S'`
 start_seconds=$(date --date="$starttime" +%s);
